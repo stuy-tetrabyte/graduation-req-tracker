c4ef7fe24477d9160214c1cd2938aa8f5135d84b
elc1798 <subliminalmau5@gmail.com>
2016-05-12 14:06:35 -0400

    Add other needed method stubs

--- utils/database_setup.py
+++ utils/database_setup.py
@@ -1,6 +1,7 @@
 import pandas
+import argparse
 
-def load_excel(filepath):
+def get_excel(filepath):
     """
     Returns a Pandas datafile that contains the contents of a Microsoft Excel
     Spreadsheet
@@ -33,9 +34,45 @@ def check_if_mysql_installed():
     """
     pass
 
-def set_up_mysql_schema():
+def is_table_set_up():
     """
-    Sets up MySQL with a table with a set schema
+    Returns True if this project's MySQL table is set up, False otherwise
     """
     pass
+
+def create_project_table(column_names):
+    """
+    Sets up MySQL with a table with a set schema given a list of column_names.
+    Does nothing if the table is already set up.
+
+    Params:
+        column_names - A list of strings containing column names
+
+    Returns:
+        None
+    """
+    pass
+
+def delete_project_table():
+    """
+    Deletes the table in MySQL that this project uses. Will do nothing if the
+    table does not yet exist.
+    """
+    pass
+
+def reset_project_table():
+    """
+    Resets the table for this project by calling 'delete' and 'setup'
+    """
+    delete_project_table()
+    create_project_table()
+
+def load_excel_file(datafile):
+    """
+    Takes a Pandas datafile and inserts the data into the project's MySQL table.
+    If the project's table is not yet created, this function will call 'create'.
+    """
+    if not is_table_set_up():
+        create_project_table()
+    # TODO: Fill in this part
 
