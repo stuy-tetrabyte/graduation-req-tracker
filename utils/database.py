d83d40988aebc3b343f5d4ab3052ad62d6987c65
elc1798 <subliminalmau5@gmail.com>
2016-05-16 09:39:54 -0400

    Please use 4 spaces rather than a tab character please

--- utils/database.py
+++ utils/database.py
@@ -8,22 +8,22 @@ assert(database_setup.is_table_set_up())
 
 
 def get_student(OSIS):
-	"""
-	get_student: returns a dictionary of the student with the given osis and all of the courses they've taken
+    """
+        get_student: returns a dictionary of the student with the given osis and all of the courses they've taken
 
-	Args: 
-		OSIS (string): 9 digit id of the student in quesiton
-	Returns:
-		Dict with student info and courses 
-		ex : {
-			name: "Student Name",
-			OSIS: "123456789",
-			term_1 : [ABC21, etc],
-			term_2 : [ABC21, etc],
-			term_3 : [ABC21, etc],
-			etc
-		}
-	"""
-	
+        Args: 
+                OSIS (string): 9 digit id of the student in quesiton
+        Returns:
+                Dict with student info and courses 
+                ex : {
+                        name: "Student Name",
+                        OSIS: "123456789",
+                        term_1 : [ABC21, etc],
+                        term_2 : [ABC21, etc],
+                        term_3 : [ABC21, etc],
+                        etc
+                }
+        """
+
 
 
