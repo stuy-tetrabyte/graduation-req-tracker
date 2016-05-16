94f0cde9a03c4e5f9f3e1b0465f4108f298b9972
elc1798 <elc1798@gmail.com>
2016-05-16 16:49:15 +0000

    Add SQLConnector class for handling MySQL queries

---
+++ utils/SQLConnector.py
@@ -0,0 +1,40 @@
+import mysql.connector
+import Constants
+
+class Connection:
+    """
+    Class to help simplify the execution of SQL queries using MySQL Connector
+    """
+    def __init__(self, db_name):
+        """
+        Sets up the MySQL connection and cursor variable
+        """
+        self.conn = mysql.connector.connect(user='root', password='test', database=db_name)
+        self.cursor = self.conn.cursor()
+
+    def execute(self, *args):
+        """
+        Executes a query and returns all resulting rows if available
+        """
+        self.cursor.execute(*args)
+        retval = self.cursor.fetchall()
+        self.conn.commit()
+        return retval
+
+    def destroy(self):
+        """
+        Destroys the internal variables of this class
+        """
+        self.conn.close()
+        del self.cursor
+        del self.conn
+
+def execute(*args):
+    """
+    Executes a single SQL command
+    """
+    c = Connection(Constants.PROJECT_DB_NAME)
+    retval = c.execute(*args)
+    c.destroy()
+    return retval
+
