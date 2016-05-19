07dadd35a907a0fb7bb060c3ebb9c074d5834608
Yicheng Wang <alex.wyc2098@protonmail.ch>
2016-05-19 15:46:56 +0000

    get studnet done

--- utils/database.py
+++ utils/database.py
@@ -30,42 +30,47 @@ class DBManager:
                     'name': "Student Name",
                     'OSIS': "123456789",
                     'grade': 12,
-                    'courses': [
-                        [<term 1 courses>],
-                        [<term 2 courses>],
-                        ...
-                        [<term n courses>]
-                    ]
+                    'passed_courses' : [<list of passed courses>],
+                    'failed_courses' : [<list of failed courses>]
                 }
+
+            the courses are stored as tuples of (<course code>, <mark received>)
         """
 
-        q = 'SELECT FIRSTNAME, LASTNAME, COURSE, MARK, GRADE, YEAR FROM %s WHERE STUDENTID = %s ORDER BY YEAR ASC' % (self.course_table, OSIS)
+        q = 'SELECT FIRSTNAME, LASTNAME, GRADE FROM %s WHERE STUDENTID = %s' % (self.course_table, OSIS)
         res = self.conn.execute(q)
 
         if (len(res) == 0):
             return None
 
-        year = int(res[0][-1])
-        course_list = []
-        current_term = []
-
-        for item in res:
-            if int(item[-1]) != year:
-                course_list.append(current_term)
-                current_term = []
-
-            current_term.append((item[2], int(item[3])))
-
-        student = {
-            "name": "%s %s" % (res[0][0], res[0][1]),
-            "OSIS": OSIS,
-            "grade": int(res[0][4]),
-            'courses': course_list
+        student_info = {
+            'name': res[0][0] + ' ' + res[0][1],
+            'OSIS': OSIS,
+            'grade': res[0][2]
         }
 
-        return student
+        q = 'SELECT COURSE, MARK, TERM, YEAR FROM %s WHERE STUDENTID = %s AND MARK >= 65 ORDER BY YEAR, TERM ASC' % (self.course_table, OSIS)
+        res = self.conn.execute(q)
+        passed_courses = []
+        if (res):
+           for item in res:
+                passed_courses.append((item[0], item[1]))
+
+
+        q = 'SELECT COURSE, MARK, TERM, YEAR FROM %s WHERE STUDENTID = %s AND MARK < 65 ORDER BY YEAR, TERM ASC' % (self.course_table, OSIS)
+        res = self.conn.execute(q)
+
+        failed_courses = []
+        if (res):
+           for item in res:
+               failed_courses.append((item[0], item[1]))
+
+        student_info['passed_courses'] = passed_courses
+        student_info['failed_courses'] = failed_courses
+
+        return student_info
 
 if __name__ == '__main__':
     db_m = DBManager(Constants.PROJECT_DB_NAME, Constants.PROJECT_TABLE_NAME)
-    print db_m.get_student('701111577')
+    print db_m.get_student('701116533')
 
